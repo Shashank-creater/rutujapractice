3dc964effc6fca3287fe074ec245c002179baa90
RR08205490 <RR08205490@sipl-210.mshome.net>
2022-01-28 16:04:07 +0530

    practice-1 demo class

---
+++ git-demo/src/main/java/com/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Shashank gadgae's rutuja");
+		String s = "rutuja";
+		String s1 = "racha";
+		System.out.println();
+		
+	}
+}
